9b8d1c3efd3c512c3fd3ca3710c6cbb6c075ed47
Marcia Vagos <57924198+marciavagos@users.noreply.github.com>
2024-11-17 20:08:52 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simple-RAG',
+    version='0.1',
+    packages=find_packages(),
+    author='Marcia Vagos',
+    author_email='marcia.vagos@gmail.com',
+    description='Simple RAG implementation',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
